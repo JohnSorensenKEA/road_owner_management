3e0e5fd45c34440cb971defe78e48b8ed1a97769
John <john383p@stud.kea.dk>
2021-05-24 13:23:47 +0200

    first commit

---
+++ src/main/java/com/example/road_owner_management/RoadOwnerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.road_owner_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadOwnerManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadOwnerManagementApplication.class, args);
+    }
+
+}
