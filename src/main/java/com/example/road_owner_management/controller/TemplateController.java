6ae5a7d89087692ce28eb16f2733866a616117f1
John <john383p@stud.kea.dk>
2021-05-25 12:18:31 +0200

    <0/

--- src/main/java/com/example/road_owner_management/controller/TemplateController.java
+++ src/main/java/com/example/road_owner_management/controller/TemplateController.java
@@ -1,4 +1,13 @@
 package com.example.road_owner_management.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TemplateController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
